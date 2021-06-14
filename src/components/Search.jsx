50ae8c28698da65b21a37c909848e0010502eed5
SatanStriike <anyone@DESKTOP-SKFU21P.localdomain>
2021-06-14 00:47:04 -0500

    full estilos e imagenes + fuente

---
+++ src/components/Search.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../assets/styles/components/Search.scss';
+
+const Search = () => (
+    <section class="main">
+        <h2 class="main__tittle">¿Que quieres ver hoy?</h2>
+        <input class="main__tittle--input" type="text" placeholder="Buscar ..." />
+    </section>
+);
+
+export default Search;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // ReactDOM empuja los componentes al DOM del html
 import ReactDOM from 'react-dom';
-import HelloWorld from './components/HelloWorld';
+import App from './container/App';
 
 // Recive dos parametros, el componente y donde quiero empujar este componente
-ReactDOM.render(<HelloWorld />,document.getElementById("app"));
+ReactDOM.render(<App />,document.getElementById("app"));
